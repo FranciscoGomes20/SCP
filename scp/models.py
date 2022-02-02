ea6c009cad14aefdf64ba05dc4eb9adff518a1a4
Cristian Suzuki <cristiansuzuki5@hotmail.com>
2022-02-02 15:20:06 -0300

    changing plural name on database models

--- scp/models.py
+++ scp/models.py
@@ -6,17 +6,29 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name = ("Categoria")
+        verbose_name_plural = ("Categorias")
+
     def __str__(self):
         return self.nome
 
 class Status(models.Model):
     nome = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = ("Status")
+        verbose_name_plural = ("Status")
     
     def __str__(self):
         return self.nome
 
 class Setor(models.Model):
     nome = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = ("Setor")
+        verbose_name_plural = ("Setores")
 
     def __str__(self):
         return self.nome
@@ -30,6 +42,10 @@ class Chamado(models.Model):
     categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, blank=True, null=True)
     status = models.ForeignKey('Status', on_delete=models.CASCADE, blank=True, null=True)
     setor = models.ForeignKey('Setor', on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        verbose_name = ("Chamado")
+        verbose_name_plural = ("Chamados")
 
     def __str__(self):
         return self.titulo
