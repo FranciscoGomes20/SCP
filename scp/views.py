495b4d68146647d7b8711321e8d094c689709520
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-02-02 22:13:06 -0500

    Create front-end

--- scp/views.py
+++ scp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def loginUser(request):
+    return render(request, 'registration/login.html')
+
+def index(request):
+    return render(request, 'dashboard.html')
