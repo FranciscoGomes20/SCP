5168e696bdf75a2a17d5c53d9d705c1a39b82bfb
Cristian Suzuki <cristiansuzuki5@hotmail.com>
2022-02-04 11:23:17 -0300

    login template and form

--- scp/views.py
+++ scp/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 def loginUser(request):
     return render(request, 'registration/login.html')
 
+@login_required
 def index(request):
     return render(request, 'dashboard.html')
