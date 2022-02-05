d70068e61f42377e80efbbd81873fe48d5641c5a
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-02-05 00:38:09 -0500

    Urls adicionadas

--- scp/urls.py
+++ scp/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.loginUser, name='login')
+    path('login', views.loginUser, name='login'),
+    path('icons', views.icons, name='icons'),
+    path('map', views.map, name='map'),
+    path('notifications', views.notifications, name='notifications'),
+    path('user', views.user, name='user'),
+    path('tables', views.tables, name='tables'),
+    path('typography', views.typography, name='typography'),
+    path('rtl', views.rtl, name='rtl'),
+    path('upgrade', views.upgrade, name='upgrade'),
 ]
--- scp/views.py
+++ scp/views.py
@@ -8,3 +8,35 @@ def loginUser(request):
 @login_required
 def index(request):
     return render(request, 'dashboard.html')
+
+@login_required
+def icons(request):
+    return render(request, 'icons.html')
+
+@login_required
+def map(request):
+    return render(request, 'map.html')
+
+@login_required
+def notifications(request):
+    return render(request, 'notifications.html')
+
+@login_required
+def user(request):
+    return render(request, 'user.html')
+
+@login_required
+def tables(request):
+    return render(request, 'tables.html')
+
+@login_required
+def typography(request):
+    return render(request, 'typography.html')
+
+@login_required
+def rtl(request):
+    return render(request, 'rtl.html')
+
+@login_required
+def upgrade(request):
+    return render(request, 'upgrade.html')
